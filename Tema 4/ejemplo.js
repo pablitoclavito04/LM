98cc9d7c1d048d0961305391772b165a8ff3d48c
Pablo <sanzaznarp@gmail.com>
2025-03-05 09:09:08 +0100

    JQuery actu

--- Tema 4/ejemplo.js
+++ Tema 4/ejemplo.js
@@ -3,5 +3,18 @@ let obj_documento = $ (document)
 obj_documento.ready(inicio)
 
 function inicio() {
-    let obj_izq=$(
+    let obj_izq=$("botonizq")
+    obj_izq.click ( fn_click_izq )
+    let obj_der=$("botonder")
+    obj_der.click ( fn_click_der )
+}
+
+function fn_click_izq() {
+    let obj_div=$("#texto")
+    obj_div.fadeOut()
+}
+
+function fn_click_der() {
+    let obj_div=$("#texto")
+    obj_div.fadeIn()
 }
